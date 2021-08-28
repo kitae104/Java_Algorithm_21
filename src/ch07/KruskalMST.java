6bd11afb07dfaed74079f4e951e24b0f484341e3
kitae104 <aqua0405@gmail.com>
2021-08-28 10:49:48 +0900

    크루스칼(Kruskal) 알고리즘

--- src/ch07/KruskalMST.java
+++ src/ch07/KruskalMST.java
@@ -76,10 +76,81 @@ public class KruskalMST
         // 트리에 추가된 간선들의 수가 (vertexCnt - 1)이 될 때까지 반복한다
         while (e < vertexCnt - 1)
         {
+        	Edge nextEdge = new Edge();
+        	nextEdge = edgeArr[i];			// 가중치가 가장 작은 간선을 선택
+        	i++;							// 반복을 위해 인덱스 증가 
         	
+        	// 선택된 간선의 정점들이 속한 부분집합 찾기 
+        	int x = find(subsets, nextEdge.src);
+        	int y = find(subsets, nextEdge.dest);
+        	
+        	// 찾은 부분집합들이 서로 다르다면 간선을 트리에 추가하고
+            // 다음에 추가할 간선을 위해 지수를 1만큼 증가시킨다.
+            // 찾은 부분집합들이 같다면 선택된 간선을 버린다
+        	if(x != y)
+        	{
+        		result[e++] = nextEdge;
+        		union(subsets, x, y);
+        	}
+        }
+        
+        // 최소 비용 신장 트리에 포함된 간선들을 가중치와 같이 출력한다
+        System.out.println("\n찾은 최소 비용 신장 트리\n");
+        System.out.println("간선\t가중치");
+        
+        for (i = 0; i < e; ++i)
+        {
+            System.out.println("(" + result[i].src + ", " + result[i].dest + ")\t  " + result[i].weight);
         }
 	}
 	
+	/**
+	 * <pre>
+	 * 1. 개요 : 경로 압축 기법을 사용하여 요소 i가 속한 부분집합을 찾는 메소드
+	 * 2. 처리내용 : 
+	 * </pre>
+	 * @param subsets
+	 * @param i
+	 * @return
+	 */
+	public int find(Subset[] subsets, int i)
+	{
+		if(subsets[i].parent != i)
+		{
+			subsets[i].parent = find(subsets, subsets[i].parent);
+		}
+		return subsets[i].parent;
+	}
+
+	/**
+	 * <pre>
+	 * 1. 개요 : x가 속한 집합과 y가 속한 집합의 합집합을 순위를 이용하여 구하는 메소드
+	 * 2. 처리내용 : 
+	 * </pre>
+	 * @param subsets
+	 * @param x
+	 * @param y
+	 */
+	public void union(Subset[] subsets, int x, int y)
+	{
+		int xRoot = find(subsets, x);
+		int yRoot = find(subsets, y);
+		
+		if(subsets[xRoot].rank < subsets[yRoot].rank)
+		{	// 순위가 낮은 트리를 순위가 높은 트리의 뿌리 노드의 바로 아래에 놓는다
+			subsets[xRoot].parent = yRoot;
+		}
+		else if(subsets[xRoot].rank > subsets[yRoot].rank)
+		{
+			subsets[yRoot].parent = xRoot;
+		}
+		else 
+		{	// 두 트리의 순위가 같다면 한 트리를 뿌리 노드로 만들고 그 트리의 순위를 1만큼 증가시킨다
+			subsets[yRoot].parent = xRoot;
+			subsets[xRoot].rank++;
+		}
+	}
+
 	public static void main(String[] args)
 	{
 		int V = 5;  // 그래프내의 정점들의 수
